93c0e32cb0ed4b2c495eb609f936e98d1e004ce0
titasimangunsong <tita.marita@si.ukdw.ac.id>
2019-09-12 15:55:06 +0700

    Initial commit

---
+++ app/src/main/java/com/proteintracker/proteintrackerapp/ProteinTrackerActivity.java
@@ -0,0 +1,14 @@
+package com.proteintracker.proteintrackerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProteinTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_protein_tracker);
+    }
+}
